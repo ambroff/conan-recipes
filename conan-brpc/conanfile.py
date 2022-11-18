ac94ff6964fda75c262a7d041f19df9b702e5445
Kyle Ambroff-Kao <kambroff@linkedin.com>
2022-11-17 21:19:30 -0800

    brpc 1.3.0

--- conan-brpc/conanfile.py
+++ conan-brpc/conanfile.py
@@ -4,8 +4,8 @@ import os
 
 class BrpcConan(ConanFile):
     name = "brpc"
-    version = "0.9.6"
-    license = "https://github.com/apache/incubator-brpc/blob/master/LICENSE"
+    version = '1.3.0'
+    license = "https://github.com/apache/incubator-brpc/blob/1.3.0/LICENSE"
     url = "https://github.com/jjkoshy/conan-recipes/conan-brpc"
     description = "An industrial-grade RPC framework used throughout Baidu"
     settings = "os", "os_build", "compiler", "build_type", "arch", "arch_build"
@@ -18,12 +18,14 @@ class BrpcConan(ConanFile):
             "shared": False,
             "with_snappy": False }
     # cmake_paths generates a file which we set as the cmake toolchain file
-    generators = ("cmake", "cmake_paths", "cmake_find_package")
+    generators = ('cmake_paths', 'CMakeDeps')
     exports_sources = ["CMakeLists.txt", "patches/*"]
 
-    requires = ("gflags/2.2.2",
-                "protobuf/3.9.1",
-                "leveldb/1.22")
+    requires = (
+        'gflags/2.2.2',
+        'protobuf/3.21.4',
+        'leveldb/1.23',
+        'openssl/1.1.1s',)
 
     def config(self):
         # must build protobuf with zlib since brpc build does not make it optional
@@ -39,27 +41,28 @@ class BrpcConan(ConanFile):
         tools.download("https://github.com/apache/incubator-brpc/archive/%s.zip" % self.version, zip_name)
         tools.unzip(zip_name)
         # TODO: move to conandata.yml
-        tools.check_md5(zip_name, "783b3b0d5b9d254a93f26ce769b00bfc")
+        tools.check_md5(zip_name, "0b2e4af0a9b61705d73a7a1957d9cd4f")
         os.unlink(zip_name)
         with tools.chdir(self.zip_folder_name):
             # TODO: switch to conandata.yml approach
-            tools.patch(patch_file="../patches/brpc-0.9.6.patch", strip=1)
+            tools.patch(patch_file="../patches/brpc-1.3.0.patch", strip=1)
 
-            # add snappy to list of libs to link with if with_snappy is true
-            if self.options.with_snappy:
-                tools.patch(patch_file="../patches/snappy.patch", strip=1)
+            # # add snappy to list of libs to link with if with_snappy is true
+            # if self.options.with_snappy:
+            #     tools.patch(patch_file="../patches/snappy.patch", strip=1)
 
     def configure_cmake(self):
         cmake = CMake(self)
 
-        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "conan_paths.cmake"
+        cmake.definitions['CMAKE_TOOLCHAIN_FILE'] = 'conan_paths.cmake'
         # TODO: the source_subfolder should be something generic like
         # src or source-subfolder, and we should rename. change
         # cmakelists.txt also
         cmake.configure(
             defs={
                 'CMAKE_POSITION_INDEPENDENT_CODE': True,
-                'protobuf_MODULE_COMPATIBLE': True
+                'protobuf_MODULE_COMPATIBLE': True,
+                'BUILD_BRPC_TOOLS': False,
             })
         return cmake
     
